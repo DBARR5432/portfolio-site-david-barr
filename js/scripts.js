1d26caa8e84b64e042771a72f10582c4866228bb
DBARR5432 <davidmbarr@gmail.com>
2024-03-02 17:20:52 -0600

    Create scripts.js
    add js code for toggling hamburger menu

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+console.log("Portfolio Site");
+
+function menuToggle() {
+    var x = document.getElementById('myNavToggle');
+    if (x.className === 'navToggle') {
+    x.className += ' responsive';
+    } else {
+        x.className = 'navToggle';
+    }
+}
